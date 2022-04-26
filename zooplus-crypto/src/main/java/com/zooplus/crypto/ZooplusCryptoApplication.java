8aa920af411e8302b0eee4da6a1b43ca4d47df82
zees007@rediffmail.com <zees007@rediffmail.com>
2022-04-26 18:30:22 +0530

    adding project files

---
+++ zooplus-crypto/src/main/java/com/zooplus/crypto/ZooplusCryptoApplication.java
@@ -0,0 +1,13 @@
+package com.zooplus.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooplusCryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZooplusCryptoApplication.class, args);
+    }
+
+}
---
+++ zooplus-crypto/src/test/java/com/zooplus/crypto/ZooplusCryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zooplus.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooplusCryptoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
